418c9e978e980d81675492dc0854ed19b2538baa
hyejungg <kimhyejung12@naver.com>
2021-11-22 16:35:33 +0900

    [fix] ci 실패 원인 찾기..
    원인은 bcrpty 모듈 설치 과정에서 발생한 것으로 판단 .... 
    1. 4개 오류 중 3개 파일에서 bcrpty 모듈을 못찾는 것으로 확인되어 해당 모듈을 externals 시킴. 
    2. 바벨로더를 추가하여 javascript 파일로 변환하도록 함

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,31 @@
+const path = require("path");
+
 module.exports = {
   target: "node",
   entry: "./src/app.js",
   output: {
-    path: __dirname + "/dist",
+    path: path.resolve(__dirname, "dist"),
     filename: "build.js",
   },
-  //   watch: true,
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+            plugins: ["@babel/plugin-syntax-dynamic-import"],
+          },
+        },
+      },
+    ],
+  },
+  stats: {
+    errorDetails: true,
+  },
+  externals: {
+    bcrypt: "bcrypt",
+  },
 };
