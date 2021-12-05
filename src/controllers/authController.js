2f9dda6a77c522c9f4749a553447ae2270cd6434
hyejungg <kimhyejung12@naver.com>
2021-12-05 14:05:53 +0900

    [add] email 검증 추가  + 회원가입과 동시에 로그인 진행해서 jwt 발급

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -5,35 +5,58 @@ require("dotenv").config({ path: "../.env" });
 
 module.exports = {
   signUp: async function (req, res) {
-    await User.signUp(req)
-      .then((result) => {
-        console.log(result);
-        res.status(200).json({
-          success: true,
-          message: "wishboard 회원가입을 성공했습니다.",
-        });
-      })
-      .catch((err) => {
-        if (err.code == "ER_DUP_ENTRY") {
-          res.status(400).json({
-            success: false,
-            message: "이미 존재하는 아이디 입니다.",
-          });
-        } else {
+    const isVaildate = await User.vaildateEmail(req);
+
+    if (!isVaildate) {
+      await User.signUp(req)
+        .then(() => {
+          passport.authenticate("local", { session: false }, (err, user) => {
+            if (err || !user) {
+              console.log(err);
+              return res.status(401).json({
+                success: false,
+                message: "wishboard 회원가입 성공 후 로그인 실패",
+              });
+            }
+            req.login(user, { session: false }, (err) => {
+              if (err) {
+                console.log(err);
+                return res.status(500).send(err);
+              }
+              const token = jwt.sign(
+                user[0].user_id,
+                process.env.JWT_SECRET_KEY
+              );
+              return res.status(201).json({
+                success: true,
+                message: "wishboard 회원가입 후 로그인 성공",
+                token: token,
+              });
+            });
+          })(req, res);
+        })
+        .catch((err) => {
+          console.log(err);
           res.status(404).json({
             success: false,
-            message: "wish board 앱 회원가입 실패",
+            message: "wishboard 앱 회원가입 실패",
           });
-        }
+        });
+    } else {
+      res.status(400).json({
+        success: false,
+        message: "이미 존재하는 아이디 입니다.",
       });
+    }
   },
   signIn: async function (req, res) {
     passport.authenticate("local", { session: false }, (err, user) => {
       if (err || !user) {
         console.log(err);
-        return res
-          .status(401)
-          .json({ success: false, message: "wish board 앱 로그인 실패" });
+        return res.status(401).json({
+          success: false,
+          message: "아이디 혹은 비밀번호를 다시 확인해주세요.",
+        });
       }
       req.login(user, { session: false }, (err) => {
         if (err) {
@@ -41,9 +64,9 @@ module.exports = {
           return res.status(500).send(err);
         }
         const token = jwt.sign(user[0].user_id, process.env.JWT_SECRET_KEY);
-        return res.status(200).json({
+        return res.status(201).json({
           success: true,
-          message: "wish board 앱 로그인 성공",
+          message: "wishboard 앱 로그인 성공",
           token: token,
         });
       });
