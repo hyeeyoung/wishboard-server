e2b9a6e7b7075e2cd9c28d34486059ca5c53cec3
hyejungg <kimhyejung12@naver.com>
2021-12-13 19:10:47 +0900

    [add] user 수정, 삭제 API 추가

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,63 +1,131 @@
 const User = require("../models/user");
-const bcrypt = require("bcryptjs");
+const logger = require("../config/winston");
+
+const TAG = "userController ";
 
 module.exports = {
-  // userSignUp: async function (req, res) {
-  //   await User.signUp(req)
-  //     .then((result) => {
-  //       console.log(result);
-  //       res.status(200).json({
-  //         success: true,
-  //         message: "wishboard 회원가입을 성공했습니다.",
-  //       });
-  //     })
-  //     .catch((err) => {
-  //       if (err.code == "ER_DUP_ENTRY") {
-  //         res.status(400).json({
-  //           success: false,
-  //           message: "이미 존재하는 아이디 입니다.",
-  //         });
-  //       } else {
-  //         res.status(404).json({
-  //           success: false,
-  //           message: "wish board 앱 회원가입 실패",
-  //         });
-  //       }
-  //     });
+  deleteUserOne: async function (req, res) {
+    await User.deleteUser(req)
+      .then((result) => {
+        if (!result) {
+          res.status(404).json({
+            success: false,
+            message: "이미 삭제된 유저 입니다.",
+          });
+        } else {
+          res.status(200).json({
+            success: true,
+            message: "유저 삭제 성공",
+          });
+        }
+      })
+      .catch((err) => {
+        logger.error(TAG + err);
+        res.status(500).json({
+          success: false,
+          message: "wishboard 서버 에러",
+        });
+      });
+  },
+  updateUserInfo: async function (req, res) {
+    if (!req.body.nickname || !req.body.profile_img)
+      return res.status(400).json({
+        success: false,
+        message: "잘못된 요청입니다.",
+      });
+    await User.updateInfo(req)
+      .then((result) => {
+        if (!result) {
+          res.status(404).json({
+            success: false,
+            message: "유저 정보를 수정할 수 없습니다.",
+          });
+        } else {
+          res.status(200).json({
+            success: true,
+            message: "유저 정보 수정 성공",
+          });
+        }
+      })
+      .catch((err) => {
+        logger.error(TAG + err);
+        res.status(500).json({
+          success: false,
+          message: "wishboard 서버 에러",
+        });
+      });
+  },
+  updateUserImage: async function (req, res) {
+    if (!req.body.profile_img)
+      return res.status(400).json({
+        success: false,
+        message: "잘못된 요청입니다.",
+      });
+    await User.updateImage(req)
+      .then((result) => {
+        if (!result) {
+          res.status(404).json({
+            success: false,
+            message: "프로필 이미지를 수정할 수 없습니다.",
+          });
+        } else {
+          res.status(200).json({
+            success: true,
+            message: "프로필 이미지 수정 성공",
+          });
+        }
+      })
+      .catch((err) => {
+        logger.error(TAG + err);
+        res.status(500).json({
+          success: false,
+          message: "wishboard 서버 에러",
+        });
+      });
+  },
+  updateUserNickname: async function (req, res) {
+    if (!req.body.nickname)
+      return res.status(400).json({
+        success: false,
+        message: "잘못된 요청입니다.",
+      });
+    await User.updateNickname(req)
+      .then((result) => {
+        if (!result) {
+          res.status(404).json({
+            success: false,
+            message: "닉네임을 수정할 수 없습니다.",
+          });
+        } else {
+          res.status(200).json({
+            success: true,
+            message: "닉네임 수정 성공",
+          });
+        }
+      })
+      .catch((err) => {
+        logger.error(TAG + err);
+        res.status(500).json({
+          success: false,
+          message: "wishboard 서버 에러",
+        });
+      });
+  },
+  // updateUserFCMToken: async function (req, res) { // TODO
+  //   await User.updateFCM(req).then().catch();
   // },
-  // userSignIn: async function (req, res) {
-  //   await User.signIn(req)
-  //     .then((result) => {
-  //       console.log(result);
-  //       if (result.length === 0)
-  //         res.status(404).json({
-  //           success: false,
-  //           message: "존재하지 않는 이메일입니다.",
-  //         });
-  //       else {
-  //         if (!bcrypt.compareSync(req.body.password, result[0].password))
-  //           res.status(404).json({
-  //             success: false,
-  //             message: "비밀번호가 일치하지 않습니다.",
-  //           });
-  //         else
-  //           res.status(200).json({
-  //             success: true,
-  //             message: "wish board 앱 로그인 성공",
-  //             data: {
-  //               user_id: result[0].user_id,
-  //               email: result[0].email,
-  //             },
-  //           });
-  //       }
-  //     })
-  //     .catch((err) => {
-  //       res.status(500).json({
-  //         success: false,
-  //         message: "wishboard 서버 에러",
-  //       });
-  //       console.log(err);
-  //       throw err;
-  //     });
-  // },
+  selectUserInfo: async function (req, res) {
+    await User.selectInfo(req)
+      .then((result) => {
+        logger.info(TAG + result);
+        res.status(200).json(result);
+      })
+      .catch((err) => {
+        logger.error(TAG + err);
+        res.status(500).json({
+          success: false,
+          message: "wishboard 서버 에러",
+        });
+      });
+  },
 };
--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,8 +1,12 @@
 const userController = require("../controllers/userController");
+const { verifyToken } = require("../middleware/auth");
 var router = require("express").Router();
 
-// router.post("/signup", userController.userSignUp);
-// router.post("/signin", userController.userSignIn);
-//@todo : 유저 삭제에 대한 api 추가하기
+router.delete("/", verifyToken, userController.deleteUserOne);
+router.put("/", verifyToken, userController.updateUserInfo);
+router.put("/image", verifyToken, userController.updateUserImage);
+router.put("/nickname", verifyToken, userController.updateUserNickname);
+// router.put("/fcm", userController.updateUserFCMToken); // TODO
+router.get("/", verifyToken, userController.selectUserInfo);
 
 module.exports = router;
