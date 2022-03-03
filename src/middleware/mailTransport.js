9b03dadf53153fe26f03fe01fe285c5319de77c0
hyejungg <kimhyejung12@naver.com>
2022-03-03 14:15:53 +0900

    [mod] naver -> google 계정 변경하여 발신자 주소 변경

--- src/middleware/mailTransport.js
+++ src/middleware/mailTransport.js
@@ -1,17 +1,14 @@
 const nodemailer = require('nodemailer');
 require('dotenv').config({ path: '../.env' });
 
-// TODO SSL 인증서 설정 후 주석 삭제 지우고 보안 포트 설정하기
-
 const mailConfig = {
-  service: 'naver',
-  host: 'smtp.naver.com',
-  port: 587,
-  // port: 465, // 보안 포트
-  // secure: true, // ture for port 465, false for other ports
+  service: 'gmail',
+  host: 'smtp.gmail.com',
+  port: 465, // 보안 포트
+  secure: true, // ture for port 465, false for other ports
   auth: {
-    user: process.env.WISHBOARD_NAVER_ID,
-    pass: process.env.WISHBOARD_NAVER_PW,
+    user: process.env.WISHBOARD_GMAIL_ID,
+    pass: process.env.WISHBOARD_GMAIL_PW,
   },
 };
 
