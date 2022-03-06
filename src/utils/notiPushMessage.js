0fcbd57ce98dcd029d286df40a6bdb6c195adc1d
hyejungg <kimhyejung12@naver.com>
2022-03-06 23:35:01 +0900

    [mod] 푸쉬 알림 메시지 형태 변경

--- src/utils/notiPushMessage.js
+++ src/utils/notiPushMessage.js
@@ -2,13 +2,21 @@ const { Strings } = require('./strings');
 
 require('dotenv').config({ path: '../.env' });
 
+// 프론트에서 NotiType 변경 시 key, value 값 통일해야함
+const notiType = {
+  RESTOCK: '재입고',
+  OPEN_DAY: '오픈일',
+  PREORDER_CLOSE: '프리오더 마감',
+  SALE_START: '세일 시작',
+  SALE_CLOSE: '세일 마감',
+};
+
 const message = {
   notification: {
     title: Strings.notiMessageTitle,
     body: '',
   },
   data: {
-    notiType: '',
     itemId: '',
     pushType: Strings.NOTI_SCREEN,
   },
@@ -16,17 +24,14 @@ const message = {
 };
 
 function dataMessage(itemNotiType, itemId, deviceFcmToken) {
-  message.notification.body = Strings.notiMessageDescription;
-  message.data.notiType = itemNotiType;
+  message.notification.body = `${notiType.itemNotiType} ${Strings.notiMessageDescription}`;
   message.data.itemId = String(itemId);
   message.token = deviceFcmToken;
   return message;
 }
 
 function dataMessageWithCount(itemNotiType, itemId, notiCount, deviceFcmToken) {
-  message.notification.body =
-    `알림 외 ${notiCount}개의 ` + Strings.notiMessageDescription;
-  message.data.notiType = itemNotiType;
+  message.notification.body = `${notiType.itemNotiType} 알림 외 ${notiCount}개의 ${Strings.notiMessageDescription}`;
   message.data.itemId = String(itemId);
   message.token = deviceFcmToken;
   return message;
