0792412b27763c1ca41dcd0bab0e19390c2ee141
hyejungg <kimhyejung12@naver.com>
2022-05-16 00:14:00 +0900

    [mod] response 형태 wishItem으로 변경

--- src/controllers/itemController.js
+++ src/controllers/itemController.js
@@ -143,11 +143,11 @@ module.exports = {
         throw new BadRequest(ErrorMessage.BadRequestMeg);
       }
       onBindParsingType(req.query.site)
-        .then((itemInfo) => {
+        .then((WishItem) => {
           res.status(StatusCode.OK).json({
             success: true,
             message: SuccessMessage.itemParse,
-            data: { itemInfo },
+            data: { WishItem },
           });
         })
         .catch((parserFailError) => next(parserFailError));
--- src/lib/parser.js
+++ src/lib/parser.js
@@ -1,9 +1,8 @@
 const axios = require('axios');
 const cheerio = require('cheerio');
+const { WishItem } = require('../dto/cartResponse');
 const { NotFound } = require('../utils/errors');
 const { ErrorMessage } = require('../utils/response');
-
-let itemInfo;
 
 const getHtml = async (url) => {
   try {
@@ -13,72 +12,153 @@ const getHtml = async (url) => {
   }
 };
 
+// ! 각 케이스별로 로그를 찍어보고 () 완전히 묶기
 const onBindParsingType = async (url) => {
   const site = String(url); // startsWith쓰려고 String으로 형변환 url은 Any 데이터 타입이므로
-  return await parsingForGeneral(url);
-  //   switch (site) {
-  //     case site.startsWith('https://store.musinsa.com/') ||
-  //       site.startsWith('https://musinsaapp.page.link/'):
-  //       return await parsingForMusinsa(site);
-  //     case site.startsWith('https://m.wconcept.co.kr/'):
-  //       return await parsingForWconcept(site);
-  //     case site.startsWith('https://m.smartstore.naver.com/'):
-  //       return await parsingForNaverStore(site);
-  //     case site.startsWith('https://www.cosstores.com/'):
-  //       return await parsingForCos(site);
-  //     default:
-  //       return await parsingForGeneral(site);
-  //   }
+  if (
+    site.startsWith('https://store.musinsa.com/') ||
+    site.startsWith('https://musinsaapp.page.link/') ||
+    site.startsWith('https://www.musinsa.com/')
+  ) {
+    return await parsingForMusinsa(site);
+  }
+  if (site.startsWith('https://m.wconcept.co.kr/')) {
+    return await parsingForWconcept(site);
+  }
+  if (
+    site.startsWith('https://m.smartstore.naver.com/') ||
+    site.startsWith('https://smartstore.naver.com/') ||
+    site.startsWith('https://search.shopping.naver.com/') ||
+    site.startsWith('https://brand.naver.com/')
+  ) {
+    return await parsingForNaver(site);
+  }
+  if (site.startsWith('https://www.cosstores.com/')) {
+    return await parsingForCos(site);
+  }
+  return await parsingForGeneral(site);
 };
 
 const parsingForGeneral = async (url) => {
-  itemInfo = { itemName: '', itemImage: '', itemPrice: '' }; // 초기화
+  const wishItem = new WishItem();
   await getHtml(url).then((html) => {
     if (html.status == 200) {
       const $ = cheerio.load(html.data);
       $('meta').each((_, el) => {
-        const key = $(el).attr('property')?.split(':')[1]; // split(':')[1]는 og 뒤 title / description 등 ...
-        if (key) {
-          const value = $(el).attr('content');
-          switch (key) {
+        const ogTag = $(el).attr('property')?.split(/:/)[1];
+        if (ogTag) {
+          const ogValue = $(el).attr('content');
+          switch (ogTag) {
             case 'title':
-              itemInfo.itemName = value;
+              wishItem.item_name = ogValue;
               break;
             case 'image':
-              if (itemInfo.itemImage == '') {
-                itemInfo.itemImage = value;
+              if (!wishItem.item_img) {
+                wishItem.item_img = ogValue;
               }
               break;
             case 'price':
             case 'Price':
-              if (itemInfo.itemPrice == '') {
-                itemInfo.itemPrice = value;
+              if (!wishItem.item_price) {
+                wishItem.item_price = ogValue;
               }
               break;
           }
         }
       });
+      const priceArray = [];
+      console.log($('div.price').text());
+      $(/[pP]rice/)?.each((idx, el) => {
+        priceArray[idx] = $(el).attr('class');
+        console.log(priceArray);
+        console.log($(el).attr('class'));
+      });
+      if (!wishItem.item_price) {
+        // WishItem.item_price = $(/^.lowestPrice*/).html();
+      }
     }
   });
-  console.log(itemInfo);
-  return itemInfo;
+  // console.log(`im general ${wishItem}`);
+  return wishItem;
 };
 
 // TODO 구현 예정
-// const parsingForMusinsa = async (url) => {
-//   return itemInfo;
-// };
+const parsingForMusinsa = async (url) => {
+  const wishItem = new WishItem();
+  let priceValue;
+  await getHtml(url).then((html) => {
+    if (html.status == 200) {
+      const $ = cheerio.load(html.data);
+      $('meta').each((_, el) => {
+        const ogTag = $(el).attr('property')?.split(/^og:/)[1];
+        if (ogTag) {
+          const ogValue = $(el).attr('content');
+          switch (ogTag) {
+            case 'title':
+              wishItem.item_name = ogValue;
+              priceValue = ogValue.split(' ');
+              break;
+            case 'image':
+              if (!wishItem.item_img) {
+                wishItem.item_img = ogValue;
+              }
+              break;
+          }
+        }
+      });
+      wishItem.item_price = priceValue[priceValue.length - 4];
+    }
+  });
+  // console.log(`im musinsa ${wishItem.item_img}`);
+  return wishItem;
+};
 
 // const parsingForWconcept = async (url) => {
-//   return itemInfo;
+//   return wishItem;
 // };
 
-// const parsingForNaverStore = async (url) => {
-//   return itemInfo;
-// };
+const parsingForNaver = async (url) => {
+  // resetWishItem();
+  const wishItem = new WishItem();
+  await getHtml(url).then((html) => {
+    if (html.status == 200) {
+      const $ = cheerio.load(html.data);
+      $('meta').each((_, el) => {
+        const ogTag = $(el).attr('property')?.split(/^og:/)[1];
+        if (ogTag) {
+          const ogValue = $(el).attr('content');
+          switch (ogTag) {
+            case 'title':
+              wishItem.item_name = ogValue;
+              priceValue = ogValue.split(' ');
+              break;
+            case 'image':
+              if (!wishItem.item_img) {
+                wishItem.item_img = ogValue;
+              }
+              break;
+          }
+        }
+      });
+      //* 앞 smartStore, 뒤 search~
+      if (!wishItem.item_price) {
+        if (
+          String(url).includes('smartstore') ||
+          String(url).includes('brand')
+        ) {
+          wishItem.item_price = $('._1LY7DqCnwR').html();
+        } else {
+          wishItem.item_price = $('.lowestPrice_num__3AlQ-').text();
+        }
+      }
+    }
+  });
+  // console.log(`im Naver ${wishItem}`);
+  return wishItem;
+};
 
 // const parsingForCos = async (url) => {
-//   return itemInfo;
+//   return WishItem;
 // };
 
 module.exports = { onBindParsingType };
