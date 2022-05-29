4504b135406ae376f2ce8565add2c314973f989e
hyejungg <kimhyejung12@naver.com>
2022-05-29 17:17:37 +0900

    [mod] production 모드인 경우에만 슬랙에러 발생하도록 코드 추가

--- src/middleware/handleError.js
+++ src/middleware/handleError.js
@@ -1,44 +1,47 @@
 const { GeneralError } = require('../utils/errors');
 const { ErrorMessage } = require('../utils/response');
+require('dotenv').config({ path: '../.env' });
 const logger = require('../config/winston');
 const Slack = require('../lib/slack');
 
 const handleErrors = (err, req, res, next) => {
   logger.error(err);
-  if (err instanceof GeneralError) {
-    if (
-      err.message === ErrorMessage.BadRequestMeg ||
-      err.message === ErrorMessage.ApiUrlIsInvalid
-    ) {
-      Slack.sendMessage({
-        color: Slack.Colors.info,
-        title: `${err.getCode()} ${err.message}`,
-        text: `[${req.method}] ${req.url} \`\`\`user-agent: ${req.header(
-          'user-agent',
-        )} \nip-adress: ${req.ip}\`\`\``,
+  if (process.env.NODE_ENV === 'production') {
+    if (err instanceof GeneralError) {
+      if (
+        err.message === ErrorMessage.BadRequestMeg ||
+        err.message === ErrorMessage.ApiUrlIsInvalid
+      ) {
+        Slack.sendMessage({
+          color: Slack.Colors.info,
+          title: `${err.getCode()} ${err.message}`,
+          text: `[${req.method}] ${req.url} \`\`\`user-agent: ${req.header(
+            'user-agent',
+          )} \nip-adress: ${req.ip}\`\`\``,
+        });
+      }
+      return res.status(err.getCode()).json({
+        success: false,
+        message: err.message,
       });
     }
-    return res.status(err.getCode()).json({
+
+    Slack.sendMessage({
+      color: Slack.Colors.danger,
+      title: 'wishboard 서버 에러',
+      text: err,
+      fields: [
+        {
+          title: 'Error Stack:',
+          value: `\`\`\`${err.stack}\`\`\``,
+        },
+      ],
+    });
+    return res.status(500).json({
       success: false,
-      message: err.message,
+      message: 'wishboard 서버 에러',
     });
   }
-
-  Slack.sendMessage({
-    color: Slack.Colors.danger,
-    title: 'wishboard 서버 에러',
-    text: err,
-    fields: [
-      {
-        title: 'Error Stack:',
-        value: `\`\`\`${err.stack}\`\`\``,
-      },
-    ],
-  });
-  return res.status(500).json({
-    success: false,
-    message: 'wishboard 서버 에러',
-  });
 };
 
 module.exports = handleErrors;
