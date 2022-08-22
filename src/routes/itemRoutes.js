98f609d8d90053ff34bc40f858b94b36f8d0b6f8
hyejungg <kimhyejung12@naver.com>
2022-08-22 16:54:35 +0900

    [mod] item 이미지 관련 코드 멀피타트 반영으로 수정

--- src/routes/itemRoutes.js
+++ src/routes/itemRoutes.js
@@ -2,12 +2,23 @@ const itemController = require('../controllers/itemController');
 const { verifyToken } = require('../middleware/auth');
 const express = require('express');
 const router = new express.Router();
+const multer = require('../config/multer');
 
-router.post('/', verifyToken, itemController.insertItemInfo);
+router.post(
+  '/',
+  verifyToken,
+  multer.upload.single('item_img'),
+  itemController.insertItemInfo,
+);
 router.get('/', verifyToken, itemController.selectItemInfo);
 router.get('/latest', verifyToken, itemController.selectItemLatest);
 router.get('/parse', itemController.parseItemInfo);
-router.put('/:item_id', verifyToken, itemController.updateItemInfo);
+router.put(
+  '/:item_id',
+  verifyToken,
+  multer.upload.single('item_img'),
+  itemController.updateItemInfo,
+);
 router.put(
   '/:item_id/folder/:folder_id',
   verifyToken,
