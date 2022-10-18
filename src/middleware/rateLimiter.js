13e2087eff486cde3c013847a43d80fbbebc28ca
hyejungg <kimhyejung12@naver.com>
2022-10-18 14:41:07 +0900

    [fix] 1초에 최대 20번 요청 가능하도록 수정

--- src/middleware/rateLimiter.js
+++ src/middleware/rateLimiter.js
@@ -1,9 +1,10 @@
 const rateLimit = require('express-rate-limit');
 
 const limiter = rateLimit({
-  // 1초에 1개의 요청만 가능하도록
-  windowMs: 1000, // 1초 ... 15 * 60 * 1000 15분
-  max: 1,
+  // 1초에 10개의 요청만 가능하도록
+  windowMs: 60000, // 1초, 60 * 1000
+  max: 10,
+  delayMs: 1000,
   standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
   legacyHeaders: false, // Disable the `X-RateLimit-*` headers
   message: 'Too many accounts created from this IP',
