c8cc723ad19630d3eb33831830cf902bb89c2316
hyejungg <kimhyejung12@naver.com>
2023-02-13 00:17:30 +0900

    [mod] 주석 삭제 및 코드 정리

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -3,22 +3,21 @@ const { ErrorMessage } = require('../utils/response');
 const { Unauthorized } = require('../utils/errors');
 require('dotenv').config({ path: '../.env' });
 
-exports.verifyToken = (req, res, next) => {
+const jwtSecret = process.env.JWT_SECRET_KEY;
+
+const verifyToken = (req, res, next) => {
   try {
     const token = req.headers.authorization.split('Bearer ')[1];
-    const jwtSecret = process.env.JWT_SECRET_KEY;
 
-    req.decoded = jwt.verify(token, jwtSecret);
+    const jwtVerify = jwt.verify(token, jwtSecret);
+    req.decoded = jwtVerify.data;
     return next();
   } catch (err) {
-    // if (err.name == "TokenExpiredError") { //TODO 현재 만료 기간 X (나중에 사용 안하면 지우기)
-    //   return res
-    //     .status(419)
-    //     .json({ success: false, message: "token 만료되었습니다." });
-    // }
-    throw new Unauthorized(ErrorMessage.requestTokenAgain);
-    // return res
-    //   .status(StatusCode.UNAUTHORIZED)
-    //   .json({ success: false, message: ErrorMessage.unValidateToken });
+    if (err.name == 'TokenExpiredError') {
+      throw new Unauthorized(ErrorMessage.expireToken);
+    }
+    throw new Unauthorized(ErrorMessage.unValidateToken);
   }
 };
+
+module.exports = { verifyToken };
