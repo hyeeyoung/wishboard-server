1b82ffab27d938b088bcb452bc0075c7342dd9bb
hyejungg <kimhyejung12@naver.com>
2023-03-17 01:13:07 +0900

    [mod] redis 패스워드 설정 추가

--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -23,6 +23,7 @@ module.exports = {
       'process.env.DB_NAME': JSON.stringify(process.env.DB_NAME),
       'process.env.REDIS_HOST': JSON.stringify(process.env.REDIS_HOST),
       'process.env.REDIS_PORT': JSON.stringify(process.env.REDIS_PORT),
+      'process.env.REDIS_PASSWORD': JSON.stringify(process.env.REDIS_PASSWORD),
       'process.env.JWT_SECRET_KEY': JSON.stringify(process.env.JWT_SECRET_KEY),
       'process.env.WISHBOARD_GMAIL_ID': JSON.stringify(
         process.env.WISHBOARD_GMAIL_ID,
