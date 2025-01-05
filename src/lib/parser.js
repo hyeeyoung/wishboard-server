88f8e05fe4028f888eda15064d9ff667ec5d541f
hyejungg <kimhyejung12@naver.com>
2025-01-05 16:29:24 +0900

    user-agent 추가

--- src/lib/parser.js
+++ src/lib/parser.js
@@ -3,10 +3,22 @@ const cheerio = require('cheerio');
 const { NotFound } = require('../utils/errors');
 const { ErrorMessage } = require('../utils/response');
 
+const userAgents = [
+  'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.4951.67 Safari/537.36',
+  'Mozilla/5.0 (Linux; U; Android 2.1; en-us; sdk Build/ERD79) AppleWebKit/530.17 (KHTML, like Gecko) Version/4.0 Mobile Safari/530.17',
+];
+
+const getRandomUserAgent = () => {
+  return userAgents[Math.floor(Math.random() * userAgents.length)];
+};
+
 const config = {
   headers: {
-    'User-Agent':
-      'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3',
+    'User-Agent': getRandomUserAgent(),
+    'Accept':
+      'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
+    'Accept-Encoding': 'gzip, deflate, br',
+    'Connection': 'keep-alive',
   },
   validateStatus: function (status) {
     return status >= 200 && status < 300;
