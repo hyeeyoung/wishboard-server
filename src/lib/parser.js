bebd005f92b019a31a6ca955e655f3bde7fe904f
hyejungg <kimhyejung12@naver.com>
2025-01-05 16:33:10 +0900

    코드 리포맷팅

--- src/lib/parser.js
+++ src/lib/parser.js
@@ -15,10 +15,10 @@ const getRandomUserAgent = () => {
 const config = {
   headers: {
     'User-Agent': getRandomUserAgent(),
-    'Accept':
+    Accept:
       'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
     'Accept-Encoding': 'gzip, deflate, br',
-    'Connection': 'keep-alive',
+    Connection: 'keep-alive',
   },
   validateStatus: function (status) {
     return status >= 200 && status < 300;
